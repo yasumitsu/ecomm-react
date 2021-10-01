f0c90d0e555d2218bfa87b628b32b30220dbbbad
Kenji <kenjiyasumitsu@gmail.com>
2021-10-01 11:58:22 +0900

    add route to /hats

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Route, Switch } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
 	return (
 		<div>
-			<HomePage />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route exact path="/hats" component={HatsPage} />
+			</Switch>
 		</div>
 	);
 }
